515139da6cf55e0422d192bce99772cb10e4ea0b
Taufan Nicola <taufanderazor@gmail.com>
2023-11-23 20:46:59 +0700

    feat: add logout page 2

--- app/src/main/java/com/example/lingga/MainActivity.kt
+++ app/src/main/java/com/example/lingga/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : AppCompatActivity() {
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
         // ini changesnya
+        //ini changes 2
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
