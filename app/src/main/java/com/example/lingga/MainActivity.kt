0fb0d5d5a31f1380003a647a2b4529c4872f3c59
taufan-mft <taufanderazor@gmail.com>
2023-11-28 20:45:02 +0700

    feat: chat feature (#4)
    * feat: chat room 1
    
    * feat: chat room finished

--- app/src/main/java/com/example/lingga/MainActivity.kt
+++ app/src/main/java/com/example/lingga/MainActivity.kt
@@ -27,8 +27,7 @@ class MainActivity : AppCompatActivity() {
         val navController = findNavController(R.id.nav_host_fragment_content_main)
         appBarConfiguration = AppBarConfiguration(navController.graph)
         setupActionBarWithNavController(navController, appBarConfiguration)
-        // ini changesnya abc
-        //ini changes 2
+
         binding.fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                 .setAction("Action", null).show()
